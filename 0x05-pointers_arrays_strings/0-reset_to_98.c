ccff718dfa1585fb0ac82837d8fbd170c19eee26
Lilysuzanna <anyadikesuzette@gmail.com>
2022-10-24 15:16:54 -0700

    function to take a pointer to an int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*reset_to_98-Takes pointer to an integer as parameter and
+* updates the value it points to 98
+* @n: updates the value of n
+* Return:0 Always
+*/
+void reset_to_98(int *n)
+{
+int temp;
+temp = *n;
+}
+int main(void);
+{
+int i = 98;
+reset_to_98(&n);
+printf("N is %d\n");
+return (0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+cazette readme
